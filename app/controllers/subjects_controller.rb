991ed2fe8720527b721a52e21a600a23c8d5bfe8
Leydi <leidyflor2007@hotmail.com>
2016-03-02 16:43:42 -0500

    Asignaturas listo

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,16 +1,40 @@
 class SubjectsController < ApplicationController
 
 def index
-		@subjects = Subject.all
+		@subject = Subject.all
 	end
 
 	def new
 		@subject = Subject.new
 	end
 
+	def edit
+		@subject = Subject.find(params[:id])
+
+	end
+
 
 	def subject_params
-		params.require(:subject).permit(:id, :message)
+		params.require(:subject).permit(:nombre, :codigo)
+	end
+
+	def create
+		@subject = Subject.new(subject_params)
+		@subject.save
+		redirect_to subjects_path
+	end
+
+	def destroy
+		@subject = Subject.find(params[:id])
+		@subject.destroy
+		redirect_to subjects_path
+	end
+
+
+	def update
+		@subject = Subject.find(params[:id])
+		@subject.update(subject_params)
+		redirect_to subjects_path
 	end
 
 end
